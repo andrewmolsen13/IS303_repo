3796aa7fb5464898e2fd430ed47277488d9e2fd2
andrewmolsen13 <olsena13@byu.edu>
2026-09-23 13:23:24 -0600

    Changes 1.1

--- class_git_practice.py
+++ class_git_practice.py
@@ -1,1 +1,8 @@
 print ("I messed something up")
+
+print("What happens if i do this?")
+
+print("Let's see what happens next") 
+
+print("I wanna commit these changes but it wont let me")
+print("Maybe I need to check my git status")
